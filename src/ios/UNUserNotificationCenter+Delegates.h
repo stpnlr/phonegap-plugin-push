8bde34d7d0452f12d84c558f20218ab4e58b8455
merrygobyebye <eric.94253@gmail.com>
2018-05-11 11:14:41 -0700

    :hankey: Add category on UNUserNotificationCenter that will allow this plugin's UNUserNotificationCenterDelegate to respond to push notification events and katzer/cordova-plugin-local-notifications's UNUserNotificationCenterDelegate to respond to local notification events

---
+++ src/ios/UNUserNotificationCenter+Delegates.h
@@ -0,0 +1,49 @@
+//
+//  UNUserNotificationCenter+Delegates.h
+//  push
+//
+//  Created by Eric Fisher on 5/11/18.
+//  Copyright © 2018 PhoneGap. All rights reserved.
+//
+
+@import UserNotifications;
+
+@interface UNUserNotificationCenter (Delegates) <UNUserNotificationCenterDelegate>
+
+/*!
+ *  @brief Add up to two delegates to observe UNUserNotificationCenter events. One for push notifications, and one for local notifications.
+ *
+ *  This property is added to enable support for this plugin to work in parallel with katzer/cordova-plugin-local-notifications. When receiving events related
+ *  to a push notification, the UNUserNotificationCenter will call this plugin's UNUserNotificationCenterDelegate methods. When receiving events related
+ *  to a local notification, the UNUserNotificationCenter will call katzer/cordova-plugin-local-notifications's UNUserNotificationCenterDelegate methods.
+ *
+ *  @warning This has only been tested with katzer/cordova-plugin-local-notifications@0.9.0-beta.2.
+ *
+ */
+@property (nonatomic, retain) NSMutableSet<id<UNUserNotificationCenterDelegate>> *delegates;
+
+/*!
+ *  @brief Always set self as the delegate, but add up to two other objects, one for this plugin and one for katzer/cordova-plugin-local-notifications.
+ *
+ *  This method will set this object as its own delegate. Subsequently it will notify the appropriate delegate in the delegates property when it receives its
+ *  own delegate events. The setDelegate method supports only two delegates, so this can only be used with exactly one other plugin that implements the
+ *  UNUserNotificationCenter delegate methods.
+ *
+ *  @warning This has only been tested with katzer/cordova-plugin-local-notifications@0.9.0-beta.2.
+ *
+ */
+- (void)swizzledSetDelegate:(id<UNUserNotificationCenterDelegate>)delegate;
+
+
+/*!
+ *  @brief Returns the appropriate delegate based on whether or not the trigger is a UNPushNotificationTrigger.
+ *
+ *  @return This plugin's UNUserNotificationCenterDelegate if the trigger is a UNPushNotificationTrigger. Otherwise it will return
+ *  katzer/cordova-plugin-local-notifications's UNUserNotificationCenterDelegate.
+ *
+ *  @warning This has only been tested with katzer/cordova-plugin-local-notifications@0.9.0-beta.2.
+ *
+ */
+- (id<UNUserNotificationCenterDelegate>)getDelegateForNotificationTrigger:(UNNotificationTrigger *)trigger;
+
+@end
---
+++ src/ios/UNUserNotificationCenter+Delegates.m
@@ -0,0 +1,126 @@
+//
+//  UNUserNotificationCenter+Delegates.m
+//  push
+//
+//  Created by Eric Fisher on 5/11/18.
+//  Copyright © 2018 PhoneGap. All rights reserved.
+//
+
+#import "UNUserNotificationCenter+Delegates.h"
+#import "AppDelegate.h"
+@import ObjectiveC;
+
+static char delegatesKey;
+
+@implementation UNUserNotificationCenter (Delegates)
+
+#pragma mark - Swizzle
+
++ (void)load
+{
+    // Exchange the setDelegate method
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        SEL originalSelector = @selector(setDelegate:);
+        SEL swizzledSelector = @selector(swizzledSetDelegate:);
+        
+        Method original = class_getInstanceMethod(class, originalSelector);
+        Method swizzled = class_getInstanceMethod(class, swizzledSelector);
+        
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzled),
+                        method_getTypeEncoding(swizzled));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(original),
+                                method_getTypeEncoding(original));
+        } else {
+            method_exchangeImplementations(original, swizzled);
+        }
+    });
+}
+
+- (void)swizzledSetDelegate:(id<UNUserNotificationCenterDelegate>)delegate
+{
+    if (self.delegate && self.delegate != self)
+    {
+        [self.delegates addObject:self.delegate];
+    }
+    
+    // Only supports two delegates: one for this plugin and one for katzer/cordova-plugin-local-notifications
+    if (self.delegates.count < 2)
+    {
+        [self.delegates addObject:delegate];
+    }
+    
+    // Because the methods are exchanged, this will call the original method
+    [self swizzledSetDelegate:self];
+}
+
+
+#pragma mark - Accessors
+
+- (NSMutableSet<id<UNUserNotificationCenterDelegate>> *)delegates
+{
+    NSMutableSet<id<UNUserNotificationCenterDelegate>> *delegates = objc_getAssociatedObject(self, &delegatesKey);
+    if (delegates == nil)
+    {
+        // Only supports two delegates: one for this plugin and one for katzer/cordova-plugin-local-notifications
+        self.delegates = [NSMutableSet setWithCapacity:2];
+    }
+    
+    return objc_getAssociatedObject(self, &delegatesKey);
+}
+
+- (void)setDelegates:(NSMutableSet<id<UNUserNotificationCenterDelegate>> *)delegates
+{
+    objc_setAssociatedObject(self, &delegatesKey, delegates, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (id<UNUserNotificationCenterDelegate>)getDelegateForNotificationTrigger:(UNNotificationTrigger *)trigger
+{
+    BOOL isPushNotification = [trigger isKindOfClass:UNPushNotificationTrigger.class];
+    
+    NSPredicate *localPredicate = [NSPredicate predicateWithBlock:^BOOL(id  _Nullable evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        // katzer/cordova-plugin-local-notifications uses its own object as the UNUserNotificationCenterDelegate object
+        return ![evaluatedObject isKindOfClass:AppDelegate.class];
+    }];
+    NSPredicate *pushPredicate = [NSPredicate predicateWithBlock:^BOOL(id  _Nullable evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        // This plugin uses the AppDelegate class as the UNUserNotificationCenterDelegate object
+        return [evaluatedObject isKindOfClass:AppDelegate.class];
+    }];
+    
+    // These will contain only one object
+    NSSet<id<UNUserNotificationCenterDelegate>> *localDelegates = [self.delegates filteredSetUsingPredicate:localPredicate];
+    NSSet<id<UNUserNotificationCenterDelegate>> *pushDelegates = [self.delegates filteredSetUsingPredicate:pushPredicate];
+    
+    return isPushNotification ? [pushDelegates anyObject] : [localDelegates anyObject];
+}
+
+
+#pragma mark - <UNUserNotificationCenterDelegate>
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+    id<UNUserNotificationCenterDelegate> delegate = [self getDelegateForNotificationTrigger:notification.request.trigger];
+    [delegate userNotificationCenter:center willPresentNotification:notification withCompletionHandler:completionHandler];
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void(^)(void))completionHandler
+{
+    id<UNUserNotificationCenterDelegate> delegate = [self getDelegateForNotificationTrigger:response.notification.request.trigger];
+    [delegate userNotificationCenter:center didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+}
+
+
+@end
